e5d4c61f78b38c026835ab96331f50dd06c13f7c
JeffAllison <itsjallison@gmail.com>
2022-05-09 16:00:37 -0700

    refactored app.js router layout, completed beaniebaby function in bb.js, dot mapped beanie baby list in beanielist.js, imported params into beaniedetail.js and created async function load for useEffect in BeanieDetail.js and created state in beaniedetail.js

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -4,6 +4,12 @@ export default function BeanieBaby({ beanieBaby }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    null
+    <>
+      <Link className="beanie-baby-list-item" to={`/beanies/${beanieBaby.id}`} />
+      <h3>{beanieBaby.title}</h3>
+      <p>{beanieBaby.animal}</p>
+      <p>{beanieBaby.astroSign}</p>
+      <img className="beanie-baby-list-image" src={beanieBaby.image} />
+    </>
   );
 }
