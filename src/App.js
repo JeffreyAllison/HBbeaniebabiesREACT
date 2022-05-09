3bc35fb9ebba61036c17c954b2d749ac2ebf2fa7
JeffAllison <itsjallison@gmail.com>
2022-05-09 14:43:59 -0700

    dot mapped beanieslist

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import BeaniesPage from './BeaniesPage';
 import BeanieDetail from './BeanieDetail';
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">home</Link>
-            </li>
-            <li>
-              <Link to="/BeaniesPage">BeaniesList</Link>
-            </li>
-            <li>
-              <Link to="/BeanieDetail">BeaniePage</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
       <div className="App">
         <Switch>
-          <Route path="/BeaniesPage">
+          <Route exact path="/">
             <BeaniesPage />
             {/* this home page route should list all the beanies */}
           </Route>
-          <Route path="/BeanieDetail">
+          <Route exact path="/beanies/:id">
             <BeanieDetail />
             {/* this route should point to a particular beanie baby by id and render that specific BeanieDetail page */}
           </Route>
