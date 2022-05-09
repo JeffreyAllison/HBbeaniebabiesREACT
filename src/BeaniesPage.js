39f531e0d20bdbf503bd29a0a75b9fca6d1b8f1e
JeffAllison <itsjallison@gmail.com>
2022-05-09 16:17:22 -0700

    added onclick formulas to next/previous page buttons in beaniespage.js

--- src/BeaniesPage.js
+++ src/BeaniesPage.js
@@ -5,32 +5,37 @@ import BeaniesList from './BeaniesList';
 
 function App() {
   const [beanieBabies, setBeanieBabies] = useState([]);
-  const [page, setPage] = useState(1);
   const perPage = 40;
-  
+  const [currentPage, setCurrentPage] = useState(1);
+
   useEffect(() => {
     async function fetch() {
-      const from = page * perPage - perPage;
-      const to = page * perPage;
+      const from = currentPage * perPage - perPage;
+      const to = currentPage * perPage;
       const beanies = await getBeanieBabies(from, to);
 
       setBeanieBabies(beanies);
     }
 
     fetch();
-  }, []); // what can you do with this array to trigger a fetch every time the page changes?
+  }, [currentPage]); // what can you do with this array to trigger a fetch every time the page changes?
 
   return (
     <>
-      <h2>Current Page {page}</h2>
-      <div className='buttons'>
+      <h2>Current Page {currentPage}</h2>
+      <div className="buttons">
         {/* on click, this button should decrement the page in state  */}
         {/* also, disable this button when you are on the first page */}
-        <button>Previous Page</button>
+        <button disabled={currentPage <= 1} onClick={() => setCurrentPage(currentPage - 1)}>
+          Previous Page
+        </button>
         {/* on click, this button should increment the page in state  */}
-        <button >Next Page</button>
+        <button disabled={currentPage >= 53} onClick={() => setCurrentPage(currentPage + 1)}>
+          Next Page
+        </button>
       </div>
       {/* pass the beanie babies into the BeaniesList component */}
+      <BeaniesList beanieBabies={beanieBabies} />
     </>
   );
 }
